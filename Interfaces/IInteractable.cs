51be8a7033e1b2dae2742191864dc22a2a7aa27c
DelsantiV <vincent.delsanti@gmail.com>
2025-08-07 18:48:08 +0200

    Update IInteractable.cs
    Better methods

--- Interfaces/IInteractable.cs
+++ Interfaces/IInteractable.cs
@@ -1,11 +1,11 @@
-using UnityEngine;
 public interface IInteractable
 {
     public string ObjectName {get;}
 
     public bool ShouldDisplayNameOnMouseOver { get; }
-
+    public void OnLookAt(Player player);
     public void OnInteract(Player player);
+    public void OnStopLookAt();
     public void OnStopInteract();
 }
 
