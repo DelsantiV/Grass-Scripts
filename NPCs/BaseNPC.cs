cc55427f0c0738f7ef4f1b83dff290cc3bd65429
Fab3737 <delsantifabien@gmail.com>
2025-08-07 12:26:26 +0200

    NPC Look at Player

--- NPCs/BaseNPC.cs
+++ NPCs/BaseNPC.cs
@@ -1,18 +1,32 @@
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
 
 public class BaseNPC : MonoBehaviour, IInteractable
 { 
     [SerializeField] protected NPCSO NPCSO;
+    [SerializeField] private Rig rig;
+    [SerializeField] private Transform headLookAtTransform;
     public float speed;
     protected Animator animator;
     public string ObjectName => NPCSO.NPCname;
     private bool isNewToPlayer;
+
+
+    private bool isLookAtPosition;
+
 
     public bool ShouldDisplayNameOnMouseOver => true;
 
     public virtual void OnInteract(Player player)
     {
         OpenNextDialogBox();
+
+        isLookAtPosition = true;
+        headLookAtTransform.position = new Vector3(player.transform.position.x, headLookAtTransform.position.y, player.transform.position.z);
+
+        Debug.Log("Coucou");
+
     }
     protected virtual void Awake()
     {
@@ -20,11 +34,21 @@ public class BaseNPC : MonoBehaviour, IInteractable
     }
     protected virtual void Update()
     {
+
+        float targetWeight = isLookAtPosition ? 1f : 0f;
+        float lerpSpeed = 2f;
+        rig.weight = Mathf.Lerp(rig.weight, targetWeight, Time.deltaTime * lerpSpeed);
+
+
         if (speed != 0)
         {
             MoveForward();
         }
+
+
+
     }
+
     private void MoveForward()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
