ad24445929eed1b1d6983eff9059e5b5365a55b8
DelsantiV <vincent.delsanti@gmail.com>
2025-08-09 00:51:09 +0200

    Update ObjectContainer.cs
    Option to parent object to container after unlock

--- Objects/ObjectContainer.cs
+++ Objects/ObjectContainer.cs
@@ -5,11 +5,22 @@ public class ObjectContainer : MonoBehaviour
     [SerializeField] protected GameObject containedObject;
     [SerializeField] protected Vector3 containedObjectPosition;
     [SerializeField] protected Vector3 containedObjectRotation;
-
+    [SerializeField] protected bool instantiateObjectOnlyOnOpen;
     protected virtual void Awake()
+    {
+        if (!instantiateObjectOnlyOnOpen)
+        {
+            SetContainedObject();
+        }
+    }
+    public virtual void Open()
+    {
+        SetContainedObject();
+    }
+    protected virtual void SetContainedObject()
     {
         containedObject.transform.parent = transform;
         containedObject.transform.SetLocalPositionAndRotation(containedObjectPosition, Quaternion.Euler(containedObjectRotation));
-        if (containedObject.TryGetComponent<Rigidbody>(out Rigidbody rb)) rb.isKinematic = true;
+        if (containedObject.TryGetComponent<InteractableObject>(out InteractableObject iobj)) iobj.shouldHaveRigidBody = false;
     }
 }
