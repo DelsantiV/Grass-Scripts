33b368d2c19593139094235e49f67b1c74ec11bb
DelsantiV <vincent.delsanti@gmail.com>
2025-08-09 22:18:34 +0200

    Update Openable.cs
    Option to disable collider when open

--- Objects/Interactables/Openable.cs
+++ Objects/Interactables/Openable.cs
@@ -1,20 +1,24 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Openable : InteractableObject
 {
     public bool isLocked;
     [SerializeField] protected bool uninteractableAfterOpen = false;
+    [SerializeField] protected bool disableColliderWhenOpen = false;
     public override bool ShouldDisplayNameOnMouseOver => isLocked;
     private Animator animator;
     private bool isOpen;
     private ObjectContainer container;
     private bool isContainer;
+    private Collider _collider;
     protected override void Awake()
     {
-        base.Awake(); 
+        base.Awake();
         animator = GetComponent<Animator>();
         isContainer = TryGetComponent<ObjectContainer>(out container);
         outline.OutlineMode = Outline.Mode.OutlineVisible;
+        TryGetComponent<Collider>(out _collider);
     }
     protected override void Start()
     {
@@ -36,15 +40,18 @@ public class Openable : InteractableObject
         if (isLocked) return;
         base.Interact(player);
         isOpen = !isOpen;
-        animator.SetTrigger(isOpen ? "Open" : "Close") ;
+        if (isOpen) Open();
+        else Close();
     }
     public void Open()
     {
         isOpen = true;
+        if (disableColliderWhenOpen && _collider != null) _collider.enabled = false;
         animator.SetTrigger("Open");   
     }
     protected void Close()
     {
+        if (disableColliderWhenOpen && _collider != null) _collider.enabled = true;
         isOpen = false;
         animator.SetTrigger("Close");
     }
