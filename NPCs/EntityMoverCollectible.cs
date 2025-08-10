6331cef4e69006b11eefcc5ac9dc26de80e314b2
DelsantiV <vincent.delsanti@gmail.com>
2025-08-11 01:12:52 +0200

    Create EntityMoverCollectible.cs
    New class for collectible entites

---
+++ NPCs/EntityMoverCollectible.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[RequireComponent(typeof(InteractableObject))]
+public class EntityMoverCollectible : EntityMover
+{
+    
+    private InteractableObject interactableObject;
+    protected override void Awake()
+    {
+        base.Awake();
+        interactableObject = GetComponent<InteractableObject>();
+    }
+    protected override void Start()
+    {
+        interactableObject.OnCollected.AddListener(StopMovement);
+        interactableObject.OnDropped.AddListener(InitializeBehaviour);
+    }
+}
