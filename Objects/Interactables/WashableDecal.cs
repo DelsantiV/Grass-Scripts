451e7895491af93d7e84d2700434126cb21d42f9
DelsantiV <vincent.delsanti@gmail.com>
2025-08-12 00:26:41 +0200

    Create WashableDecal.cs
    New class to create decals that can be washed by the player

---
+++ Objects/Interactables/WashableDecal.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+[RequireComponent(typeof(DecalProjector))]
+[RequireComponent(typeof(Collider))]
+public class WashableDecal : MonoBehaviour, IInteractable
+{
+    [SerializeField] Material decalMaterial;
+    [SerializeField] Material decalMaterialSelected;
+    [SerializeField] int keyIDToWash;
+    [SerializeField] private float washCooldown = 1f;
+    [SerializeField] private float washStep = 0.25f;
+    [SerializeField] private Animator soapAnimator;
+    public string ObjectName => string.Empty;
+    private new Collider collider;
+    public bool ShouldDisplayNameOnMouseOver => false;
+    private DecalProjector projector;
+    private InteractableObject soap;
+    private Player player;
+    private void Awake()
+    {
+        projector = gameObject.GetOrAddComponent<DecalProjector>();
+        collider = GetComponent<Collider>();
+        projector.material = decalMaterial;
+        if (soapAnimator == null) soapAnimator = GetComponentInChildren<Animator>();
+    }
+
+    public void OnInteract(Player player)
+    {
+        if (!player.IsCurrentObjectKey(keyIDToWash) && keyIDToWash != 0) return;
+        projector.fadeFactor -= washStep;
+        if (projector.fadeFactor == 0f) 
+        { 
+            Destroy(gameObject);
+        }
+        else
+        {
+            if (keyIDToWash != 0)
+            {
+                soap = player.TakeObject();
+                this.player = player;
+                AnimateSoap();
+            }
+            StartCoroutine(CoolDown());
+        }
+    }
+
+    public void OnLookAt(Player player)
+    {
+        projector.material = decalMaterialSelected;
+    }
+
+    public void OnStopInteract(Player player)
+    {
+        player = null;
+    }
+
+    public void OnStopLookAt(Player player)
+    {
+        if (projector != null)
+        {
+            projector.material = decalMaterial;
+        }
+    }
+    private IEnumerator CoolDown()
+    {
+        projector.material = decalMaterial;
+        collider.enabled = false;
+        yield return new WaitForSeconds(washCooldown);
+        collider.enabled = true;
+        if (soap != null)
+        {
+            soap.TryCollectObject(player);
+        }
+
+    }
+    private void AnimateSoap()
+    {
+        soap.transform.parent = soapAnimator.transform;
+        soap.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+        soapAnimator.SetTrigger("StartWashing");
+    }
+}
