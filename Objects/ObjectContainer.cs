9457a094a7eef2045c8ba8398e016f637561db6c
DelsantiV <vincent.delsanti@gmail.com>
2025-08-13 23:25:59 +0200

    Update ObjectContainer.cs
    Now use struct ContainedObject, holding an object, its position and its rotation (relative to the parent)
    Better methods to add object

--- Objects/ObjectContainer.cs
+++ Objects/ObjectContainer.cs
@@ -1,33 +1,70 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ObjectContainer : MonoBehaviour
 {
-    [SerializeField] protected GameObject containedObject;
-    [SerializeField] protected Vector3 containedObjectPosition;
-    [SerializeField] protected Vector3 containedObjectRotation;
+    [SerializeField] protected List<ContainedObject> initialObjects;
+    public int maxNumberOfObjects;
     [SerializeField] protected bool instantiateObjectOnlyOnOpen;
+    protected List<ContainedObject> containedObjects;
+    public bool isFull { get => containedObjects.Count >= maxNumberOfObjects; }
+    public int currentNumberOfObjects { get => containedObjects.Count; }
     protected virtual void Awake()
     {
+        containedObjects ??= new List<ContainedObject>();
+        if (maxNumberOfObjects == 0) maxNumberOfObjects = containedObjects.Count;
         if (!instantiateObjectOnlyOnOpen)
         {
-            SetContainedObject();
+            SetAllObjects();
         }
     }
     public virtual void Open()
     {
-        if (instantiateObjectOnlyOnOpen) SetContainedObject();
+        if (instantiateObjectOnlyOnOpen) SetAllObjects();
     }
-    protected virtual void SetContainedObject()
+    public virtual void SetAllObjects()
     {
-        containedObject.transform.parent = transform;
-        containedObject.transform.SetLocalPositionAndRotation(containedObjectPosition, Quaternion.Euler(containedObjectRotation));
-        if (containedObject.TryGetComponent<InteractableObject>(out InteractableObject iobj))
+        foreach (ContainedObject containedObject in initialObjects) AddContainedObject(containedObject, true);
+    }
+    public virtual void AddContainedObject(ContainedObject containedObject, bool force = false)
+    {
+        if (isFull && !force) return;
+        containedObjects.Add(containedObject);
+        containedObject.iobj.transform.parent = transform;
+        if (containedObject.position != Vector3.zero) containedObject.iobj.transform.SetLocalPositionAndRotation(containedObject.position, containedObject.rotation);
+        bool addRb = containedObject.iobj.shouldHaveRigidBody;
+        containedObject.iobj.OnCollected.AddListener(() => OnContainedObjectCollected(containedObject, addRb));
+        if (containedObject.iobj.shouldHaveRigidBody)
         {
-            if (iobj.shouldHaveRigidBody)
-            {
-                iobj.OnCollected.AddListener(() => iobj.shouldHaveRigidBody = true);
-                iobj.shouldHaveRigidBody = false;
-            }
+            Debug.Log("coucou");
+            containedObject.iobj.shouldHaveRigidBody = false;
+            if (containedObject.iobj.TryGetComponent<Rigidbody>(out Rigidbody rb)) Destroy(rb); 
         }
+    }
+    private void OnContainedObjectCollected(ContainedObject obj, bool addRigidbody)
+    {
+        Debug.Log(addRigidbody);
+        if (addRigidbody) obj.iobj.shouldHaveRigidBody = true;
+        containedObjects.Remove(obj);
+        obj.iobj.OnCollected.RemoveAllListeners();
+    }
+}
+[System.Serializable]
+public struct ContainedObject
+{
+    public InteractableObject iobj;
+    public Vector3 position;
+    public Quaternion rotation;
+    public ContainedObject(InteractableObject iobj, Vector3 position, Vector3 rotation)
+    {
+        this.iobj = iobj;
+        this.position = position;
+        this.rotation = Quaternion.Euler(rotation);
+    }
+    public ContainedObject(InteractableObject iobj, Vector3 position, Quaternion rotation)
+    {
+        this.iobj = iobj;
+        this.position = position;
+        this.rotation = rotation;
     }
 }
