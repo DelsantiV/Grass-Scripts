c0ce3095efefd1a661eb2613973f4e069947bfd7
DelsantiV <vincent.delsanti@gmail.com>
2025-08-14 23:20:46 +0200

    Update ShopCheckout.cs
    Now an object receiver

--- Objects/Interactables/ShopCheckout.cs
+++ Objects/Interactables/ShopCheckout.cs
@@ -1,27 +1,15 @@
-using System.Collections.Generic;
-using UnityEngine;
 
-public class ShopCheckout : MonoBehaviour
+public class ShopCheckout : ObjectReceiver
 {
-    public List<ShopObject> boughtObjects;
-    private void OnTriggerEnter(Collider other)
+    public override bool IsObjectValid(InteractableObject obj)
     {
-        if (other.TryGetComponent<ShopObject>(out ShopObject obj))
-        {
-            boughtObjects.Add(obj);
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("StoreObject") && other.TryGetComponent<ShopObject>(out ShopObject obj))
-        {
-            if (boughtObjects.Contains(obj)) boughtObjects.Remove(obj);
-        }
+        if (obj == null) return false;
+        return obj.TryGetComponent<ShopObject>(out _);
     }
 
     public void CheckOut(out int prize)
     {
         prize = 0;
-        foreach (ShopObject obj in boughtObjects) prize += obj.prize;
+        foreach (ShopObject obj in currentObjects.ConvertAll(obj => obj.iobj.gameObject.GetComponent<ShopObject>())) prize += obj.prize;
     }
 }
