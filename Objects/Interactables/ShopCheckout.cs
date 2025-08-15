c8a3f9d94ade362db366bfed37ad5b8aebad1b15
DelsantiV <vincent.delsanti@gmail.com>
2025-08-15 11:09:22 +0200

    Update ShopCheckout.cs
    Better output

--- Objects/Interactables/ShopCheckout.cs
+++ Objects/Interactables/ShopCheckout.cs
@@ -1,15 +1,19 @@
 
+using System.Collections.Generic;
+
 public class ShopCheckout : ObjectReceiver
 {
+    public List<ShopObject> boughtObjects => currentObjects.ConvertAll(obj => obj.iobj.gameObject.GetComponent<ShopObject>());
     public override bool IsObjectValid(InteractableObject obj)
     {
         if (obj == null) return false;
         return obj.TryGetComponent<ShopObject>(out _);
     }
 
-    public void CheckOut(out int prize)
+    public List<ShopObject> CheckOut(out int prize)
     {
         prize = 0;
-        foreach (ShopObject obj in currentObjects.ConvertAll(obj => obj.iobj.gameObject.GetComponent<ShopObject>())) prize += obj.prize;
+        foreach (ShopObject obj in boughtObjects) prize += obj.prize;
+        return boughtObjects;
     }
 }
