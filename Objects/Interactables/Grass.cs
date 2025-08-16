41851701eac10c24c84eb39904ac7e42c91c0525
DelsantiV <vincent.delsanti@gmail.com>
2025-08-16 02:08:06 +0200

    Update Grass.cs
    Sets a world message

--- Objects/Interactables/Grass.cs
+++ Objects/Interactables/Grass.cs
@@ -7,7 +7,7 @@ public class Grass : InteractableObject
     protected override void Interact(Player player)
     {
         base.Interact(player);
-        CanvasManager.Instance.CloseWorldMessage();
+        player.SetWorldMessage("It's good to feel some grass ! You had even forgotten what the green color was like...");
         Destroy(borders);
         Destroy(gameObject);
     }
