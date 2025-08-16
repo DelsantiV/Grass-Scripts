46d46dad29e89c4a70c2627c9fd0fa81025cc8c4
DelsantiV <vincent.delsanti@gmail.com>
2025-08-16 23:00:04 +0200

    Update ShopCheckout.cs
    Open barrier on checkout

--- Objects/Interactables/ShopCheckout.cs
+++ Objects/Interactables/ShopCheckout.cs
@@ -1,9 +1,10 @@
-
+using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 
 public class ShopCheckout : ObjectReceiver
 {
+    [SerializeField] private Openable barrier;
     public List<ShopObject> boughtObjects => currentObjects.ConvertAll(obj => obj.iobj.gameObject.GetComponent<ShopObject>());
     public List<InteractableObject> boughtInteractableObjects => currentObjects.ConvertAll(obj => obj.iobj.gameObject.GetComponent<InteractableObject>());
     public int prize => boughtObjects.Sum(obj => obj.prize);
@@ -15,5 +16,6 @@ public class ShopCheckout : ObjectReceiver
     public void CheckOut()
     {
         container.DestroyAllObjects();
+        barrier.Open();
     }
 }
