8f80dc7f3793f321b76d23e5b688b3298dca21f8
DelsantiV <vincent.delsanti@gmail.com>
2025-08-16 16:39:53 +0200

    Update BagObject.cs
    Ability to merge bags

--- Objects/Interactables/BagObject.cs
+++ Objects/Interactables/BagObject.cs
@@ -6,4 +6,16 @@ public class BagObject : InteractableObject
 
     public List<int> keyIDs;
 
+    public override void OnInteract(Player player)
+    {
+        if (player.currentObject != null) 
+        { 
+            if (player.currentObject is BagObject bag)
+            {
+                keyIDs.AddRange(bag.keyIDs);
+                Destroy(bag.gameObject);
+            }
+        }
+        return;
+    }
 }
