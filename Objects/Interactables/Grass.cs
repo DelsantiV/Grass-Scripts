dd5cc2463921917edebb66c36a5a326824db9bf5
DelsantiV <vincent.delsanti@gmail.com>
2025-08-16 16:24:04 +0200

    Update Grass.cs
    New message

--- Objects/Interactables/Grass.cs
+++ Objects/Interactables/Grass.cs
@@ -7,7 +7,8 @@ public class Grass : InteractableObject
     protected override void Interact(Player player)
     {
         base.Interact(player);
-        player.SetWorldMessage("It's good to feel some grass ! You had even forgotten what the green color was like...");
+        AudioManager.Instance.GoToNextMusic();
+        player.SetWorldMessage("It's good to feel some grass ! You had even forgotten what the green color was like... Your soul was lost in the virtual world. Touching grass makes you want to help people around");
         Destroy(borders);
         Destroy(gameObject);
     }
